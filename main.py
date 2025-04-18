6d20e99ade1bb9cbfa4332c8c5f392f2420eade4
kolibri <owink@kth.se>
2025-04-18 23:48:24 +0200

    updated the API in main.py to allow for movie reccs.

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import json
-from dataclasses import asdict
 from typing import Union, List, Generator
-import time
 
 from fastapi import FastAPI, Depends
-from fastapi.responses import JSONResponse
-from starlette.responses import HTMLResponse
 
 from domain.Movie import Movie
 from repositories.MovieRepository import InMemoryMovieRepository, SQLMovieRepository
@@ -16,11 +11,11 @@ from repositories.SimilarityRepository import (
 from repositories.UserMoviePreferenceRepository import SQLUserMoviePreferenceRepository
 from repositories.UserRepository import SQLUserRepository
 from services.MovieService import MovieService
+from services.RecommendationService import RecommendationService
 from services.SimilarityService import SimilarityService
 from services.UserMoviePreferenceService import UserMoviePreferenceService
 from services.UserService import UserService
 from db import get_connection
-from utils import init_db, get_movie_repo, get_movie_service
 
 app = FastAPI()
 MOVIE_CSV_PATH = "Data/archive/movies_metadata.csv"
@@ -37,6 +32,13 @@ sim_service = SimilarityService(similarity_repo, mov_service)
 
 user_repo = SQLUserRepository(DB_PATH)
 user_service = UserService(user_repo)
+
+usr_movie_repo = SQLUserMoviePreferenceRepository(DB_PATH)
+pref_svc = UserMoviePreferenceService(usr_movie_repo)
+
+rec_svc = RecommendationService(user_service, pref_svc, sim_service, mov_service)
+
+user_id = user_service.get_user_id_by_username("john")
 
 
 # sim_service.calculate_pairwise_similarity(num_movies=1000)
@@ -83,9 +85,12 @@ def get_user_movie_service(
     yield svc
 
 
+def get_recommendation_service() -> Generator[RecommendationService, None, None]:
+    return rec_svc
+
+
 @app.post("/users/")
 def create_user(username: str, svc: UserService = Depends(get_user_service)):
-
     user_added_bool = svc.add_user()
     if user_added_bool:
         return f"User {username} added successfully"
@@ -100,6 +105,7 @@ def rate_movie(
     movie_opinion: int,  # 0 for dislike, 1 for like
     user_svc: UserService = Depends(get_user_service),
     pref_svc: UserMoviePreferenceService = Depends(get_user_movie_service),
+    rec_svc: RecommendationService = Depends(get_recommendation_service),
 ):
     user_id = user_svc.get_user_id_by_username(username)
 
@@ -107,12 +113,41 @@ def rate_movie(
         return f"User {username} not found."
     else:
         movie_title = pref_svc.add_user_preference(user_id, movie_id, movie_opinion)
+
+        rec_svc.update_user_embedding_vector(user_id, movie_id)
         if movie_opinion:
             return f"User {username} registered as liking the movie {movie_title[0]}."
         if movie_opinion == 0:
             return (
                 f"User {username} registered as disliking the movie {movie_title[0]}."
             )
+
+
+@app.get("/get_recommendation")
+def get_recommendation(
+    username: str,
+    user_svc: UserService = Depends(get_user_service),
+    rec_svc: RecommendationService = Depends(get_recommendation_service),
+):
+    user_id = user_svc.get_user_id_by_username(username)
+    if user_id is None:
+        return f"User {username} not found."
+    return rec_svc.get_recommendation(user_id, 5)
+
+
+@app.get("/user/seen_movies")
+def get_seen_movies(
+    username: str,
+    user_svc: UserService = Depends(get_user_service),
+    pref_svc: UserMoviePreferenceService = Depends(get_user_movie_service),
+) -> List[str]:
+    user_id = user_svc.get_user_id_by_username(username)
+    movies = pref_svc.get_seen_movies(user_id)
+    if movies:
+        movie_keys, movie_titles = zip(*movies)
+        return movie_titles
+    else:
+        return "No seen movies found."
 
 
 @app.get("/all_movies")
