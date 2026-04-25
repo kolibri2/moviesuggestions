18270e3c14a47fe804fe33f17a0b3d02abb8f0ff
kolibri <owink@kth.se>
2026-04-25 22:04:42 +0200

    update vpc name

--- infra/main.tf
+++ infra/main.tf
@@ -12,7 +12,7 @@ resource "aws_ecr_repository" "movie_rec_repo" {
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
   tags = {
-    Name = "main"
+    Name = "movie-recommendation-vpc"
   }
 }
 
