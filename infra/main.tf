9edf5108d17c2258a555d4f537d44a1c27659aec
Oscar Wink <oscar.wink@ericsson.com>
2026-04-27 15:38:53 +0200

    add security group

--- infra/main.tf
+++ infra/main.tf
@@ -39,3 +39,29 @@ resource "aws_route_table_association" "public" {
   subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.public.id
 }
+
+# security group
+
+resource "aws_security_group" "movie_recommendation_sg" {
+  name        = "movie_recommendation_sg"
+  description = "allow inbound traffic on p8000 and all outbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  tags = {
+    Name = "movie_recommendation_sg"
+  }
+
+  ingress {
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
